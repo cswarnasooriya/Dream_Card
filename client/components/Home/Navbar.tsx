5f51f7739b64a66bffede94d55bb9880ba2f3f6b
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-07-13 01:06:47 +0530

    Navbar Created and Updated

--- client/components/Home/Navbar.tsx
+++ client/components/Home/Navbar.tsx
@@ -1,4 +1,6 @@
+import { navLinks } from '@/constants/Constants'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -10,6 +12,23 @@ const Navbar = () => {
             <Image src='/images/logo.png' alt='logo here'
             width={180} height={180}
             />
+
+            {/* Nav Links */}
+            <div className="lg:flex hidden items-center space-x-10">
+
+                {navLinks.map((link) => {
+                    return(
+                        <Link href={link.url} key={link.id}>
+                            <p className="text-lg font-semibold cursor-pointer hover:text-blue-800 transition-all duration-200 text-gray-900">
+                                {link.label}
+                            </p>
+                        </Link>
+                    )
+                })}
+
+            </div>
+
+            {/* icons */}
 
         </div>
       
---
+++ client/constants/Constants.ts
@@ -0,0 +1,31 @@
+ export const navLinks = [
+    {
+        id:1,
+        url:"#",
+        label:"Home",
+    },
+
+    {
+        id:2,
+        url:"/about",
+        label:"About",
+    },
+
+    {
+        id:3,
+        url:"/explor",
+        label:"Explor",
+    },
+
+    {
+        id:4,
+        url:"/resources",
+        label:"Resources",
+    },
+
+    {
+        id:5,
+        url:"/contact",
+        label:"Contact",
+    },
+ ];
