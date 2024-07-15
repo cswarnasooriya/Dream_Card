c5332ebfe21f9ec0bd6ad2037e88fa965a7cd001
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-07-15 17:29:21 +0530

    Category section created

--- client/components/Home/Home.tsx
+++ client/components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero/Hero'
 import HotBid from './HotBid/HotBid'
 import TopCollection from './TopCollection/TopCollection'
+import Category from './Category/Category'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <HotBid/>
       <TopCollection/>
+      <Category/>
     </div>
   )
 }
