fd0118efe241cfbccafbb6fbcdf19c2682994c8c
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-07-15 23:25:36 +0530

    Footer created

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
 
 const font = Poppins({ 
   weight:['100','200','300','400','500','600','700','800','900'],
@@ -27,6 +28,8 @@ export default function RootLayout({
         {/*Here display any page of web aplication */}
 
         {children}
+
+        <Footer/>
         
       </body>
     </html>
